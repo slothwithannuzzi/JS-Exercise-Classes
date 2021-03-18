3cdae5cbf665d88b19b4f972c51eecdb295d903c
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2021-03-18 15:25:38 -0500

    completed task5

--- index.js
+++ index.js
@@ -175,8 +175,25 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
+ class Student extends Lambdasian {
+   constructor(studArr){
+      super(studArr);
+      this.previousBackground = studArr.previousBackground;
+      this.className = studArr.className;
+      this.favSubjects = studArr.favSubjects;
+   }
+
+   listSubjects() {
+      return `Loving ${this.favSubjects}!`;
+   }
+
+   PRAssignment(subject) {
+     return `${this.name} has submitted a PR for ${subject}`;
+   }
+
+   sprintChallenge(subject) {
+     return `${this.name} has begun sprint challenge on ${subject}`
+   }
  }
   
   /*
